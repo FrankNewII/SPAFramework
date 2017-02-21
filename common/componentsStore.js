c31ea5183a83412d95e8439f583a9aa5056d917e
Chunikhin Artem <chunikhin.artem92@gmail.com>
2017-02-21 22:35:50 +0200

    change comnponent init

--- common/componentsStore.js
+++ common/componentsStore.js
@@ -7,10 +7,9 @@
   var availableComponents = {};
 
   components.add = add;
-  components.get = get;
   components.init = init;
 
-  function init(name) {
+  function init(name, element, parentComponent) {
 
     var applyArguments = [null];
     var args = Array.apply(null, arguments);
@@ -23,16 +22,7 @@
       throw new Error("Component is not available:" + name);
     }
 
-    return new (Function.prototype.bind.apply(availableComponents[name], applyArguments));
-  }
-
-  function get(name) {
-
-    if(!availableComponents[name]) {
-      throw new Error("Component is not available:" + name);
-    }
-
-    return availableComponents[name];
+    return new availableComponents[name](element, parentComponent);
   }
 
   function add(name, fn) {
@@ -43,6 +33,59 @@
 
     availableComponents[name] = fn;
   }
-  
+
+  function upEvent(name, data, obj) {
+    if(!obj.parent) return;
+
+    var parent = obj.parent;
+
+    while(parent) {
+      if(parent.events._listeners[name]) {
+        for(var a = 0; a < parent.events._listeners[name].length; a++) {
+          parent.events._listeners[name][a](data);
+        }
+      }
+
+      parent = parent.parent;
+    }
+  }
+
+  function downEvent(name, data, object) {
+    if(object.events._listeners && object.events._listeners[name]) {
+      for(var i = 0; i < object.events._listeners[name]; i++) {
+        object.events._listeners[name][i](data);
+      }
+    }
+
+    if(object._childrens) {
+      for(var i = 0; i < object._childrens.length; i++) {
+        downEvent(name, data, object._childrens[i]);
+      }
+    }
+  }
+
+  function modifyObject(obj) {
+
+    Object.defineProperties(a, {
+      'nonwritable': {
+        value: {},
+        enumerable: false,
+        configurable: false,
+        writable: false
+      }});
+
+    obj.prototype._listeners = [];
+    obj.prototype.on = function (name, cb) {
+      this._listeners[name].push(cb);
+    }
+
+    obj.prototype.events.up = function (name, data, obj) {
+      upEvent(name, data, obj);
+    }
+
+    obj.prototype.events.down = function (name, data, obj) {
+      upEvent(name, data, obj);
+    }
+  }
 
 })(window)
