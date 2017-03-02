9f850cd306805e9716ae52c9b0b5498085c2c2e0
Chunikhin Artem <chunikhin.artem92@gmail.com>
2017-03-02 14:37:23 +0200

    add array functions

--- common/functions.js
+++ common/functions.js
@@ -4,9 +4,13 @@
   window.common = window.common || {};
   var functions = window.common.functions = window.common.functions || {};
 
+  functions.array = {};
+  functions.array.forEach = forEach;
+  functions.array.extend = extend;
   /*Additional function for inside use or use in users application
   * In Additional functions will be most common function, like foreach, extend, recursion forEach, etc.
   * */
+
   function forEach(obj, fn) {
     if(obj || obj === null) {
       if(typeof (obj) == 'function') {
@@ -22,6 +26,12 @@
     } else {
       throw new Error('This argument can\'t be iterated', obj);
     }
+  }
+
+  function extend(obj1, obj2) {
+    forEach(obj2, function (v, k) {
+      obj1[k] = v;
+    });
   }
 
 })(window)
