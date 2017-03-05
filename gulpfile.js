7ef5a34434b83ecf49819964d7744cf634060653
FrankNewII <at20vrdg>
2017-03-05 02:37:39 +0200

    set livereloading, server

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,34 @@
 var gulp = require('gulp');
 var uglify = require('gulp-uglifyjs');
 var sass = require('gulp-sass');
+var server = require('gulp-server-livereload');
 
-gulp.task('default', ['framework-js', 'application-js']);
+var config = {
+    frameworkPath: 'common/**/*.js',
+    applicationPath: 'application/**/*.js'
+};
 
 gulp.task('framework-js', function () {
-  gulp.src('common/**/*.js')
-    .pipe(uglify())
-    .pipe(gulp.dest('framework-dist/'));
+    gulp.src('common/**/*.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('dist/js'));
 });
 
 gulp.task('application-js', function () {
-  gulp.src('application/**/*.js')
-    .pipe(uglify())
-    .pipe(gulp.dest('application-dist/js'));
+    gulp.src('application/**/*.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('dist/js'));
 });
+
+gulp.task('webserver', function () {
+    gulp.src('dist')
+        .pipe(server({
+            livereload: true,
+            directoryListing: true,
+            open: true
+        }));
+});
+
+gulp.task('watch', ['application-js', 'framework-js']);
+
+gulp.task('default', ['webserver', 'watch']);
