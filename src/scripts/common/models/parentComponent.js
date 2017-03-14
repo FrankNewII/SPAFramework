032e0d9c22da38b65c477c3f30543d6cac87e830
FrankNewII <at20vrdg>
2017-03-14 18:03:37 +0200

    change init services logic(add params in init function)

--- src/scripts/common/models/parentComponent.js
+++ src/scripts/common/models/parentComponent.js
@@ -4,6 +4,7 @@
     common.models.add('parentComponent', parent);
 
 
+
     /*
      * Почему так - все просто, на данный момент мой контроллер на елементе не знает об елементе(DOM),
      * я вынужден вызывать эту зависимость в момент инициализации контроллера
@@ -11,7 +12,7 @@
      * и для того, чтобы конечному пользователю все зависимости использовать одинаково
      * - я вынужден принять такое решение
      * */
-    function parent(parentComponent) {
+    function parent(element, parentComponent) {
         return function () {
             return parentComponent;
         };
